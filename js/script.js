75f85b23bbaa63a5bbb63bceacfad5a0f9487931
Murilo Apolonio <60797968+muriloapolo@users.noreply.github.com>
2021-11-13 12:26:52 -0300

    Removido alguns espaços em branco
    Nenhuma alteração real/significativa. remoção de espaços e sinais de comentários

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ setInterval(mostraHora, 1000);
 
 */
 
-//Cópia por que eu sou um incapaz
+
 
 let min1;
 let seg1;
@@ -52,6 +52,7 @@ function geraDados() {
     //display = document.getElementById('display');
     //display.innerHTML = `${min1} : ${seg1}`
 
+
 }
 geraDados();
 
@@ -59,26 +60,32 @@ function startContador() {
 
     min1 = 24;
     seg1 = 59;
+
+
     displayMinutes.innerHTML = min1;
     displaySeconds.innerHTML = seg1;
     btnPlay.style.pointerEvents = 'none';
 
     //inica a contagem dos minutos e segundos
-    var minutesInterval = setInterval(timerContadorMinutes, 60000);
-    var secondsInterval = setInterval(timerContadorSeconds, 1000);
+    minutesInterval = setInterval(timerContadorMinutes, 60000);
+    secondsInterval = setInterval(timerContadorSeconds, 1000);
 
     //Função responsável por deduzir os minutos
     function timerContadorMinutes() {
         min1--;
         displayMinutes.innerHTML = min1;
+
+
     }
 
     //Função reponsável por deduzir os segundos
     function timerContadorSeconds() {
+
         seg1--;
         displaySeconds.innerHTML = seg1;
 
         if (seg1 <= 0) {
+
             if (min1 <= 0) {
                 clearInterval(minutesInterval);
                 clearInterval(secondsInterval);
@@ -87,8 +94,11 @@ function startContador() {
                 fimContador.play();
                 btnPlay.style.pointerEvents = 'auto';
             }
+
             seg1 = 60;
+
         }
+
     }
 }
 
@@ -99,4 +109,5 @@ function resetContador() {
     seg1 = 0;
     displayMinutes.innerHTML = '0' + min1;
     displaySeconds.innerHTML = '0' + seg1;
+    btnPlay.style.pointerEvents = 'auto';
 }
